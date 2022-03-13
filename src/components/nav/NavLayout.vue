334631565dfbadb8366cd8555ea765836b4b1f5f
우성호 <wooncloud@hotmail.co.kr>
2022-03-13 13:26:10 +0900

    nav 스타일 변경

--- src/components/nav/NavLayout.vue
+++ src/components/nav/NavLayout.vue
@@ -3,7 +3,10 @@
     <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand" href="#" @click="onNavRouterHandler($event, main.name)">
-          {{main.title}}
+          <div class="navbar-brand-wrap">
+            <div class="navbar-logo"></div>
+            <div>{{main.title}}</div>
+          </div>
         </a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <i class="bi bi-list"></i>
@@ -38,6 +41,21 @@
   #mainNav .navbar-toggler {
     padding: 0.3rem 0.8rem !important;
     font-size: x-large !important;
+  }
+  .navbar-brand-wrap {
+    display: flex;
+  }
+  .navbar-brand-wrap > .navbar-logo {
+      background: url('@/assets/logo.png') no-repeat scroll 50% 50% /  contain;
+      height: 30px;
+      width: 30px;
+      margin-right: 4px;
+    }
+  @media (min-width: 992px) {
+    .navbar-brand-wrap > .navbar-logo {
+      height: 42px;
+      width: 42px;
+    }
   }
   .menu-list {
     margin: 20px 0 10px 0;
