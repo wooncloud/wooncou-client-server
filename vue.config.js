58fedbc7defbcae5f43f81419a238da40bf50872
우성호 <wooncloud@hotmail.co.kr>
2022-03-14 00:46:28 +0900

    config 일부 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: '운쿠 :: 추천 & 가이드 & 팁 컬렉션',
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
